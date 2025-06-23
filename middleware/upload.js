d8709163a7dd87530834ca332e6de16938b44e78
Abrar-sahib <abrarjamil906@gmail.com>
2025-06-23 21:26:27 +0500

    aaa

--- middleware/upload.js
+++ middleware/upload.js
@@ -32,7 +32,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 20 * 1024 * 1024 }, // 5MB
 });
 
 // Allow only cnicFront and cnicBack
