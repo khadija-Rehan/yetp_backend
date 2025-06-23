b54c110603da823bb2c8564f7302e393fe08c4a6
Abrar-sahib <abrarjamil906@gmail.com>
2025-06-23 21:30:29 +0500

    aaa

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ console.log("PORT",PORT);
 // Apply CORS middleware
 app.use(cors());
 
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
