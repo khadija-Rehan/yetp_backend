87a0f0b98c5731717d40b3304f14650e49720520
Abrar-sahib <abrarjamil906@gmail.com>
2025-07-29 20:36:28 +0500

    updages

--- controllers/scholarshipController.js
+++ controllers/scholarshipController.js
@@ -33,7 +33,7 @@ exports.applyForScholarship = async (req, res) => {
     }
 
     // Verify that the provided data matches the user's data
-    if (user.email !== email || user.cnic !== cnic || user.fullName !== fullName) {
+    if (user.email !== email || user.cnic !== cnic) {
       return res.status(400).json({ message: "User data does not match with registered user" });
     }
 
